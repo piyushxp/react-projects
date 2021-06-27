e2473cba385e039093702651f0b47768820af0fe
piyushxp <piyushmahapatra001@gmail.com>
2021-06-27 20:59:57 +0530

    behavior of navbar upon scrolling

--- netflix/src/components/navbar/Navbar.jsx
+++ netflix/src/components/navbar/Navbar.jsx
@@ -1,14 +1,24 @@
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./navbar.scss";
 
 function Navbar() {
+	const [isScrolled, setIsScrolled] = useState(false);
+	console.log(isScrolled);
+	useEffect(() => {
+		window.onscroll = () => {
+			setIsScrolled(window.pageYOffset === 0 ? false : true);
+		};
+
+		return () => (window.onscroll = null);
+	}, []);
+
 	const imageUrl =
 		"https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg";
 	const accountImageUrl =
 		"https://images.pexels.com/photos/7116213/pexels-photo-7116213.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
 	return (
-		<div className='navbar'>
+		<div className={isScrolled ? "navbar scrolled" : "navbar"}>
 			<div className='container'>
 				<div className='left'>
 					<img src={imageUrl} alt='' className='' />
